2dd0c4ca524ce990e690ac16e3971eeeeecba95a
HristiinaI <hristinabg01@gmail.com>
2020-01-16 19:15:20 +0200

    simple event create form

---
+++ eventure-client/server.js
@@ -0,0 +1,19 @@
+// // const express = require('nest')
+// // const app = nest()
+// // const bodyParser = require('body-parser')
+// // const cors = require('cors')
+
+
+// // app.use(cors())
+// // app.use(bodyParser.urlencoded({ extended: true }))
+// // app.use(bodyParser.json())
+
+
+// app.post('/add-review', function (req, res) {
+//   pusher.trigger('rotten-pepper', 'new-movie-review', req.body)
+//   res.sendStatus(200)
+// })
+
+// app.listen(port, function () {
+//   console.log('Node app is running at localhost:' + 8080)
+// })
