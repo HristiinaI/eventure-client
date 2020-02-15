b3209b1501760af807fb07aa6442c2e7ec643ea2
Teodora Yovcheva <t.k.yovcheva@gmail.com>
2020-02-15 15:59:06 +0200

    sugnUp form to create users, added signIn form

--- eventure-client/pages/signIn.js
+++ eventure-client/pages/signIn.js
@@ -1,14 +1,12 @@
 import React, {Component} from 'react';
-import Register from '../components/SignUp';
 import 'bootstrap/dist/css/bootstrap.css';
-import SignUp from '../components/SignUp';
 import SignIn from '../components/SignIn';
 
 export default class App extends Component {
   render()  {
     return (
         <div className = "col-md-6">
-          <h1>Welcome to Eventure!</h1>
+          <SignIn />
         </div>
     );
   }
