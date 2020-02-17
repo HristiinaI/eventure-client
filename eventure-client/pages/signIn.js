c090eea2826ba4ff4a5115c253225513a37870a6
Teodora Yovcheva <t.k.yovcheva@gmail.com>
2020-02-17 21:50:20 +0200

    moved logic for signUp && signIn from components to pages

--- eventure-client/pages/signIn.js
+++ eventure-client/pages/signIn.js
@@ -1,12 +1,30 @@
 import React, {Component} from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
-import SignIn from '../components/SignIn';
+import Link from 'next/link';
+import Router from 'next/router';
+import { Container, Col, Button, Form, FormGroup, Label, Input } from 'reactstrap';
 
 export default class SignInPage extends Component {
   render()  {
     return (
         <div className = "col-md-6">
-          <SignIn />
+          <Container className = "App">
+                <h2>Sign In</h2>
+                <Form className = "form" onSubmit = {this.handleSubmit}>
+                      <FormGroup>
+                        <Label>Email</Label>
+                        <Input type="email" className = "form-control" placeholder="Email"/>
+                      </FormGroup>
+                      <FormGroup>
+                        <Label>Password</Label>
+                        <Input type="password" className = "form-control" placeholder="Password"/>
+                      </FormGroup>
+                  <Button type="submit" className="btn btn-primary btn-block" onClick={() => Router.push('/profile')}>Sign In</Button>
+                    <p className="forgot-password text-center">
+                        Don't have an account yet? <Link href="signUp">Sign up</Link>
+                    </p>           
+                </Form>
+              </Container>
         </div>
     );
   }
