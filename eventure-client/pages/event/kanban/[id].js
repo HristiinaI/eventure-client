3b65e0b9107d2bb65efeca858f72fb48147a0556
HristiinaI <hristinabg01@gmail.com>
2020-05-04 01:04:39 +0300

    add come drag and drop

--- eventure-client/pages/event/kanban/[id].js
+++ eventure-client/pages/event/kanban/[id].js
@@ -1,21 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
-import styled from 'styled-components';
-import ListAdder from '../../../components/Kanaban/ListAdder';
-import ListTitleTextarea from '../../../components/Kanaban/ListTitleTextarea';
-import CardTextarea from '../../../components/Kanaban/CardTextarea';
-import ListTitleButton from '../../../components/Kanaban/ListTitleButton';
-import Button from '../../../components/Kanaban/Button';
+import Header from '../../../components/Board/Header';
+import { DndProvider } from "react-dnd";
+import Backend from "react-dnd-html5-backend";
+import Item from "../../../components/Board/Item";
+import DropWrapper from "../../../components/Board/DropWrapper";
+import Col from "../../../components/Board/Col";
+import { data, statuses } from "../../../data/index";
 
-class Board extends React.Component{
-  render(){
-    return(
-      <>
-      <ListAdder></ListAdder>
-      </>
-    );
-  }
-}
+const Board = () => {
+  const [items, setItems] = useState(data);
+
+  const onDrop = (item, monitor, status) => {
+      const mapping = statuses.find(si => si.status === status);
+
+      setItems(prevState => {
+          const newItems = prevState
+              .filter(i => i.id !== item.id)
+              .concat({ ...item, status, icon: mapping.icon });
+          return [ ...newItems ];
+      });
+  };
+
+  const moveItem = (dragIndex, hoverIndex) => {
+      const item = items[dragIndex];
+      setItems(prevState => {
+          const newItems = prevState.filter((i, idx) => idx !== dragIndex);
+          newItems.splice(hoverIndex, 0, item);
+          return  [ ...newItems ];
+      });
+  };
+
+  return (
+      <div className={"row"}>
+          {statuses.map(s => {
+              return (
+                  <div key={s.status} className={"col-wrapper"}>
+                      <h2 className={"col-header"}>{s.status.toUpperCase()}</h2>
+                      <DropWrapper onDrop={onDrop} status={s.status}>
+                          <Col>
+                              {items
+                                  .filter(i => i.status === s.status)
+                                  .map((i, idx) => <Item key={i.id} item={i} index={idx} moveItem={moveItem} status={s} />)
+                              }
+                          </Col>
+                      </DropWrapper>
+                  </div>
+              );
+          })}
+      </div>
+  );
+};
+
 
 Board.getInitialProps = async function (context) {
   const { id } = context.query;
